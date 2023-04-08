952e093087070141be84929d716bcc932fda1f72
opaleboreale <opaleboreale@protonmail.com>
2023-04-08 18:39:18 +0200

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mferracc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 01:12:48 by mferracc          #+#    #+#             */
+/*   Updated: 2022/11/22 08:43:36 by mferracc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdint.h>
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# include <string.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	ft_bzero(void *s, size_t n);
+
+#endif
